a742e7aab13cf8b7cf6ab01dadd9de80cc239a7b
IvashinMaxim <ivashinmaximsir@gmail.com>
2025-06-17 13:04:59 +0300

    start 0.0

---
+++ src/main/java/com/example/websiteauto/WebSiteAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websiteauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSiteAutoApplication.class, args);
+    }
+
+}
